7a1716d0ad8c50cb2f3d84339c09fe7e196cd5fe
Colton Lavalette <coltonlavalette@gmail.com>
2025-05-05 14:39:15 -0400

    Initial Commit

---
+++ Project_6A/Project_6AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6AApp.swift
+//  Project_6A
+//
+//  Created by Colfefe on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_6AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_6ATests/Project_6ATests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6ATests.swift
+//  Project_6ATests
+//
+//  Created by Colfefe on 5/5/25.
+//
+
+import Testing
+@testable import Project_6A
+
+struct Project_6ATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
