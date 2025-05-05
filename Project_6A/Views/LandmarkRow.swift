f17987e7c61819fda4314af8890295b88c999422
Colton Lavalette <coltonlavalette@gmail.com>
2025-05-05 18:10:34 -0400

    Create LandmarkRow view to display landmark details.

---
+++ Project_6A/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  Project_6A
+//
+//  Created by Colfefe on 5/5/25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
