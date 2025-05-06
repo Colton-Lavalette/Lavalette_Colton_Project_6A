84988ad53c6beb208814c0bf60cad632746ab1db
Colton Lavalette <coltonlavalette@gmail.com>
2025-05-05 22:07:43 -0400

    Add app icon. Create reusable Badge view with hexagonal background, gradient fill, and repeated rotated mountain symbol

---
+++ Project_6A/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Project_6A
+//
+//  Created by Colton Lavalette on 5/5/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
